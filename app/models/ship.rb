c5cdf7b95716c0d108c3a549426ec4d1e85ffffb
Khang <khang.le27@gmail.com>
2019-03-12 17:39:33 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(ship_hash)
+    @name = ship_hash[:name]
+    @type = ship_hash[:type]
+    @booty = ship_hash[:booty]
+
+    @@all << self
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+
 end
